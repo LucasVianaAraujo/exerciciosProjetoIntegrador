d9427ebae78579c1d8a192ea3c362a58e53c515b
Lucas Viana <luks79180@gmail.com>
2026-03-29 21:34:31 -0300

    1.6

--- src/exercicioSemaforo.java
+++ src/exercicioSemaforo.java
@@ -1,3 +1,23 @@
-public class exercicioSemaforo {
+import java.util.Scanner;
 
+public class exercicioSemaforo {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Pedestres para atravessar?");
+        Boolean situacao1 = sc.nextBoolean();
+
+        System.out.println("Veículos na pista?");
+        Boolean situacao2 = sc.nextBoolean();
+
+        if (situacao1 && situacao2) {
+            System.out.println("Situação: Prioridade é dos veículos");
+        } else {
+            if (situacao1 && !situacao2) {
+                System.out.println("Situação: Prioridade é dos pedestres");
+            } else {
+                System.out.println("Situação: Farol continua igual");
+            }
+        }
+    }
 }
